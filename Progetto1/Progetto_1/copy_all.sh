9fe8fb36470cbc3d5dbe3c342584f0fa3c666cd4
Federico Bianchi <72462475+fwhite73@users.noreply.github.com>
2023-05-25 10:41:28 +0200

    pulizia

--- Progetto1/Progetto_1/copy_all.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-scp -i ~/.ssh/pem/fb_bd_2023.pem -r Job1/* hadoop@ec2-3-216-31-6.compute-1.amazonaws.com:~/progetto1/job1
-scp -i ~/.ssh/pem/fb_bd_2023.pem -r Job2/*.sh hadoop@ec2-3-216-31-6.compute-1.amazonaws.com:~/progetto1/job2
-scp -i ~/.ssh/pem/fb_bd_2023.pem -r Job2/HIVE/comandi.hql hadoop@ec2-3-216-31-6.compute-1.amazonaws.com:~/progetto1/job2/HIVE/comandi.hql
-scp -i ~/.ssh/pem/fb_bd_2023.pem -r Job2/MR/codes/* hadoop@ec2-3-216-31-6.compute-1.amazonaws.com:~/progetto1/job2/MR
-scp -i ~/.ssh/pem/fb_bd_2023.pem -r Job2/SPARK/* hadoop@ec2-3-216-31-6.compute-1.amazonaws.com:~/progetto1/job2/SPARK
